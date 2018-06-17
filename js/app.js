f4f5858eb70160d5e959e381a40e47c02911861d
Maria Skampoura <maria.skampoura@gmail.com>
2018-06-17 02:49:31 +0100

    Committing js file

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 /*
  * Create a list that holds all of your cards
  */
-var cardsArray = [];
+var matchingCardsArray = [];
 var openCardsArray = [];
+var allMovesArray =[];
 var myCards = document.getElementById("myDeck");
 var cardsM = myCards.getElementsByTagName("li");
 var innerCard = myCards.getElementsByTagName("i");
 var restartBtn = document.getElementById("restartBtn");
+var moves = document.getElementById("movesNr");
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -17,7 +19,6 @@ var restartBtn = document.getElementById("restartBtn");
 restartBtn.addEventListener('click', function() {
 window.location.reload(true);
 });
-
 //Shuffle function from http://stackoverflow.com/a/2450976
 window.onload = function shuffle(xcardsArray) {
   var currentIndex = xcardsArray.length, temporaryValue, randomIndex;
@@ -30,18 +31,18 @@ window.onload = function shuffle(xcardsArray) {
   }
   return xcardsArray;
 }
+//Function to add event listener and change the state of the cards
 window.onload = function addCardsId() {
-  console.log("cards is called");
-  //Function to add event listener and change the state of the card
+  numberOfMoves();
   //adding an id to all cards
   for (let i = 0; i < cardsM.length; i++) {
     cardsM[i].id = i;
-    console.log(cardsM[i]);
-    cardsArray.push(cardsM[i]);
     //adding an event listener to all cards
     cardsM[i].addEventListener("click", function () {
       //Change the class of the card to be visible after click
       cardsM[i].className = "card open show";
+      allMovesArray.push(cardsM[i]);
+      numberOfMoves();
       //New array to store card class name
       openCardsArray.push(cardsM[i].firstElementChild.className);
       if (openCardsArray.length === 2) {
@@ -50,12 +51,14 @@ window.onload = function addCardsId() {
     });
   }
 }
+//Function to check if cards are of equal value
 function checkEquality() {
   if (openCardsArray[0] === openCardsArray[1]) {
-    console.log('equals');
     for (let i = 0; i < cardsM.length; i++) {
       if (cardsM[i].firstElementChild.className === openCardsArray[0]) {
         cardsM[i].className = "card match";
+        //pushing all matching elements to matchingCardsArray
+        matchingCardsArray.push(cardsM[i]);
       }
     }
     for (let i = 0; i < cardsM.length; i++) {
@@ -63,30 +66,37 @@ function checkEquality() {
         cardsM[i].className = "card match";
       }
     }
+    //empty openCardsArray
     openCardsArray = [];
   }
   if (openCardsArray[0] !== openCardsArray[1]) {
-    console.log("not equals");
-
     for (let i = 0; i < cardsM.length; i++) {
       if (cardsM[i].firstElementChild.className !== openCardsArray[0] && cardsM[i].className === "card open show") {
 
         setTimeout(function () {
 
           cardsM[i].className = "card";
-        }, 1500);
+        }, 1000);
       }
     }
     for (let i = 0; i < cardsM.length; i++) {
       if (cardsM[i].firstElementChild.className !== openCardsArray[1] && cardsM[i].className === "card open show") {
-        console.log("show2");
+
         setTimeout(function () {
 
           cardsM[i].className = "card";
-        }, 1500);
+        }, 1000);
 
       }
     }
+    //empty openCardsArray
     openCardsArray = [];
   }
 }
+
+//Function to count the number of moves
+function numberOfMoves() {
+let m = (allMovesArray.length);
+ moves.innerHTML= m + "";
+}
+
