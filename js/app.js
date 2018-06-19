51deaec23c801adee0f50f3d8d4548d2b1bc3092
Maria Skampoura <maria.skampoura@gmail.com>
2018-06-19 17:40:02 +0100

    Committing multiple files

--- js/app.js
+++ js/app.js
@@ -112,4 +112,45 @@ function matchingElements () {
   }
 }
 
+//Function to add timer from https://jsfiddle.net/Daniel_Hug/pvk6p/ from Daniel Hug
+var stopwatch = document.getElementById('stopwatch'),
+    seconds = 0, minutes = 0, hours = 0,
+    t;
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    stopwatch.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+    timer();
+}
+
+function timer() {
+    t = setTimeout(add, 1000);
+    if (matchingCardsArray.length === 16) {
+      clearTimeout(t);
+    }
+}
+timer();
+
+
+// /* Start button */
+// start.onclick = timer;
+
+// /* Stop button */
+// stop.onclick = function() {
+//     clearTimeout(t);
+// }
+
+// /* Clear button */
+// clear.onclick = function() {
+//   stopwatch.textContent = "00:00:00";
+//     seconds = 0; minutes = 0; hours = 0;
+// }
+
 
