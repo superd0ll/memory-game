0b18f991a1ac1c8a5dcb49334029ba004800348e
Maria Skampoura <maria.skampoura@gmail.com>
2018-06-19 15:40:40 +0100

    Committing multiple files

--- js/app.js
+++ js/app.js
@@ -89,8 +89,9 @@ function checkEquality() {
 //Function to count the number of moves
 function numberOfMoves() {
 let m = (parseInt(allMovesArray.length/2));
- moves.innerHTML= m + "";
+moves.innerHTML= m + "";
 }
+
 //Function to count the number of matching elements
 //Calling files to create winner screen with fireworks.
 //Code taken from https://codepen.io/funxer/pen/qKRQoj
