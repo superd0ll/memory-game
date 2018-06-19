718541acb62d3c72b903b956692565b2e574f181
Maria Skampoura <maria.skampoura@gmail.com>
2018-06-19 15:46:00 +0100

    Committing css file

--- js/app.js
+++ js/app.js
@@ -85,13 +85,11 @@ function checkEquality() {
     openCardsArray = [];
   }
 }
-
 //Function to count the number of moves
 function numberOfMoves() {
 let m = (parseInt(allMovesArray.length/2));
 moves.innerHTML= m + "";
 }
-
 //Function to count the number of matching elements
 //Calling files to create winner screen with fireworks.
 //Code taken from https://codepen.io/funxer/pen/qKRQoj
