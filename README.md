647bf886a0c09ec27617bfe83355123367af7427
Maria Skampoura <maria.skampoura@gmail.com>
2018-06-20 20:35:29 +0100

    Committing readme resize image

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ The Memory Game Project by <a href="https://udacity.com">Udacity</a> is all abou
 * Live version
 
 ## About the game
-![memory game](https://github.com/superd0ll/memory-game/blob/master/img/memory-game.png| width=48)
+<img src="https://github.com/superd0ll/memory-game/blob/master/img/memory-game.png" alt="memory game" style="width:250px;height:35000px;">
 This memory game has been created with vanilla JavaScript with no support by any external JavaScript library such as for example jQuery.
 The memory game has been created according to the instructions given by <a href="https://udacity.com">Udacity</a>.
 For instructions about playing the game please see below.
--- js/app.js
+++ js/app.js
@@ -1,44 +1,51 @@
-/*** Declare all variables
- * matchingCardsArray contains matching cards*/
+/*
+ Declare all variables
+*/
+//matchingCardsArray contains matching cards
 var matchingCardsArray = [];
-/***openCardsArray contains the cards that are currently open*/
+//openCardsArray contains the cards that are currently open
 var openCardsArray = [];
-/**allMovesArray contains all elements that have been opened*/
+//allMovesArray (contains all elements that have been opened)
 var allMovesArray =[];
-/**not sure if I need this (so far not)*/
-var myCards = document.getElementById("myDeck");
-/**using querySelecor to get node values*/
+//using querySelecor to get node values
 var cardsM = document.querySelectorAll("li");
 var innerCard = document.querySelectorAll("i");
 var ul = document.querySelector("ul");
-/**selecting elements by id (id assigned into the index.html file)*/
+//selecting elements by id (id assigned into the index.html file)
 var restartBtn = document.getElementById("restartBtn");
 var moves = document.getElementById("movesNr");
+var starOne=document.getElementById("star1");
+var starTwo=document.getElementById("star2");
+var starThree=document.getElementById("star3");
 
-/**Add event listener to the repeat button*/
+
+//Add event listener to the restart button
 restartBtn.addEventListener('click', function() {
 window.location.reload(true);
 });
 
-/**Function to add event listener and change the state of the cards*/
+// Add event listener to change card's classes
 window.onload = function addCardsId() {
   numberOfMoves();
   gameTime();
-  /**Shuffle from https://stackoverflow.com/questions/7070054/javascript-shuffle-html-list-element-order*/
-  /**this method allows the shuffling of node elements*/
+  /**
+   * Shuffle from https://stackoverflow.com/questions/7070054/javascript-shuffle-html-list-element-order
+   * this method allows
+   * the shuffling of node elements
+   * */
   for (var i = ul.children.length; i >= 0; i--) {
     ul.appendChild(ul.children[Math.random() * i | 0]);
   }
-  /**adding an id to all cards*/
+  //adding an id to all cards
   for (let i = 0; i < cardsM.length; i++) {
     cardsM[i].id = i;
-    /**adding an event listener to all cards*/
+    //adding an event listener to all cards
     cardsM[i].addEventListener("click", function () {
-      /**change the class of the card to be visible after click*/
+      //change the class of the card to be visible after click
       cardsM[i].className = "card open show";
       allMovesArray.push(cardsM[i]);
       numberOfMoves();
-      /**using openCardsArray to store card class name*/
+      //using openCardsArray to store card class name
       openCardsArray.push(cardsM[i].firstElementChild.className);
       if (openCardsArray.length === 2) {
         checkEquality();
@@ -46,13 +53,15 @@ window.onload = function addCardsId() {
     });
   }
 }
-/**Function to check if cards are of equal value*/
+/**
+ * Check if cards are of equal value
+ */
 function checkEquality() {
   if (openCardsArray[0] === openCardsArray[1]) {
     for (let i = 0; i < cardsM.length; i++) {
       if (cardsM[i].firstElementChild.className === openCardsArray[0]) {
         cardsM[i].className = "card match";
-        /**pushing all matching elements to matchingCardsArray*/
+        //pushing all matching elements to matchingCardsArray
         matchingCardsArray.push(cardsM[i]);
       }
     }
@@ -61,9 +70,10 @@ function checkEquality() {
         cardsM[i].className = "card match";
       }
     }
-    /**calling function to show winner screen*/
-    matchingElements ();
-    /**empty openCardsArray*/
+
+    //calling matchingElements to show winner screen
+    matchingElements();
+    //empty openCardsArray
     openCardsArray = [];
   }
   if (openCardsArray[0] !== openCardsArray[1]) {
@@ -86,39 +96,71 @@ function checkEquality() {
 
       }
     }
-    /**empty openCardsArray*/
+
+    //empty openCardsArray
     openCardsArray = [];
   }
+  allStars();
 }
-/**Function to count the number of moves*/
+/**
+ * Count the number of moves
+ * */
 function numberOfMoves() {
   let m = (parseInt(allMovesArray.length/2));
   moves.innerHTML= m + "";
   matchingElements();
   }
-/**Function to count the number of matching elements*/
+/**
+ * Count the number of matching elements
+ */
 function matchingElements() {
-  let over = (matchingCardsArray.length);
-  if (over===16 && (allMovesArray.length) === 16) {
-  /**showing message window with stats*/
+  //over equals the length of matchingCardsArray
+var over = (matchingCardsArray.length);
+if (over===16 && (allMovesArray.length) === 16) {
+  //showing message window with stats
   window.onload = setTimeout(function(){window.confirm("Congratulations! You won! It took you " + stopwatch.textContent + " time, and " + (allMovesArray.length/2) + " moves. You won 3 stars!");
   window.location.href = "index.html";
   }, 500);
 }
-else if (matchingCardsArray.length === 16 && ((allMovesArray.length) > 16) && ((allMovesArray.length) <= 40)) {
-    /**showing message window with stats*/
+else if (over === 16 && ((allMovesArray.length) > 16) && ((allMovesArray.length) <= 40)) {
+    //showing message window with stats
     window.onload = setTimeout(function(){window.confirm("Congratulations! You won! It took you " + stopwatch.textContent + " time, and " + (allMovesArray.length/2) + " moves. You won 1 star!");
     window.location.href = "index.html";
   }, 500);
 }
-else if (matchingCardsArray.length === 16 && ((allMovesArray.length) > 40)) {
-    /**showing message window with stats*/
+else if (over === 16 && ((allMovesArray.length) > 40)) {
+
+    //showing message window with stats
     window.onload = setTimeout(function(){window.confirm("Congratulations! You won! It took you " + stopwatch.textContent + " time, and " + (allMovesArray.length/2) + " moves. You won 2 stars!");
-  window.location.href = "index.html";
+    window.location.href = "index.html";
   }, 500);
 }
 }
-/**Function gameTime to add timer from https://jsfiddle.net/Daniel_Hug/pvk6p/ (by Daniel Hug)*/
+/**
+ * Change the number of stars
+ * according to the number of
+ * moves
+ */
+function allStars() {
+if ((allMovesArray.length/2) === 16) {
+starOne.className = "fa fa-star";
+starTwo.className = "fa fa-star";
+starThree.className ="fa fa-star";
+}
+
+else if (((allMovesArray.length/2) >16) && ((allMovesArray.length/2)<40)) {
+  starThree.className ="fa fa-star-o";
+}
+else if((allMovesArray.length/2)>=40) {
+starTwo.className = "fa fa-star-o";
+starThree.className ="fa fa-star-o";
+}
+}
+/**
+ * Function gameTime to add timer
+ * from https://jsfiddle.net/Daniel_Hug/pvk6p/
+ * (by Daniel Hug)
+ * */
 function gameTime() {
 var stopwatch = document.getElementById('stopwatch'),
     seconds = 0, minutes = 0, hours = 0,
@@ -144,5 +186,3 @@ function timer() {
 }
 timer();
 }
-/**end of gameTime */
-
